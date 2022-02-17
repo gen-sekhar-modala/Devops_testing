60295419a187d2a4e97b6b6b310102bbda6f282a
thangaraj <703302184@genpact.com>
2022-02-17 16:04:06 +0530

    new sql

---
+++ sql/E.sql
@@ -0,0 +1,5 @@
+CREATE OR REPLACE TABLE `data-engineering-gcp-practice.Example_dataset1.Emp_department`
+(
+  ID INT64 NOT NULL OPTIONS(description="Emp id"),
+  Department STRING OPTIONS(description="dep name")
+);;;;;
