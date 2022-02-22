1dec3e455d706dd858011144f36195ad71f51b7e
sekhar modala <99725133+gen-sekhar-modala@users.noreply.github.com>
2022-02-22 21:53:41 +0530

    Create hitcurl.sh

---
+++ hitcurl.sh
@@ -0,0 +1,1 @@
+curl --request GET --url 'https://kapoorakashgenpact.atlassian.net/rest/api/3/issue/{DEV-1}' --user 'kapoorakash1985@gmail.com:Zn6JNvb6j6PURA9Hn9bi540D' --header 'Accept: application/json'
