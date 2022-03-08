911e9c9bc6b3ca75b08da057d70d3dfa46f2d7bb
sekhar modala <99725133+gen-sekhar-modala@users.noreply.github.com>
2022-03-08 04:22:22 -0600

    Update test.py

--- test.py
+++ test.py
@@ -2,5 +2,4 @@ import os
 EnvironmentID=os.environ.get("EnvironmentID")
 PipelineName=os.environ.get("PipelineName")
 print("project....=",PipelineName)
-print("SRK........{}",EnvironmentID)
-print("98765........")
+print("EnvironmentID.....=",EnvironmentID)
